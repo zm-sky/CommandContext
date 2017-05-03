4b2edb2745ea6b6f0431e3bb90d850506b52812a
zmsky <hiresky3201@gmail.com>
2017-05-03 02:07:52 -0700

    algunas cosas modificadas.

---
+++ src/command/CommandContext.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import java.util.List;
+
+/**
+ *
+ * @author zippy
+ */
+public interface CommandContext{
+    public void registrarTurno(int turno);
+    public void iniciarJuego(String frase);
+    public void avanzarTurno();
+    public void setGanador(String ganador);
+    public void agregarLetra(Character letra);
+}
--- src/command/Context.java
+++ src/command/Context.java
@@ -9,6 +9,29 @@ package command;
  *
  * @author zippy
  */
-public abstract class Context {
+public abstract class Context implements CommandContext{
+    /**
+     * Contiene el contexto actual en el cual los comandos
+     * se estaran ejecutando.
+     */
+    private static Context currentContext;
+    
+    /**
+     * Setea el contexto actual en el cual los comandos 
+     * se estaran ejecutando.
+     * 
+     * @param context 
+     */
+    public static void setContext(Context context){
+        currentContext = context;
+    }
+    
+    /**
+     * Regresa el contexto actual
+     * @return 
+     */
+    public static Context getContext(){
+        return currentContext;
+    }
     
 }
